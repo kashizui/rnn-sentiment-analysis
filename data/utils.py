8d10d37974f58b83a035ce692c3a9eabc1f044c7
Nicolas Cserepy <cserepy@stanford.edu>
2016-05-22 00:53:42 -0700

    word2vec

--- data/utils.py
+++ data/utils.py
@@ -4,7 +4,7 @@ def get_sentiment(val):
 	return int(val >= 0.5)
 
 def format_data(dataset):
-	print dataset[:10]
+	print(dataset[:10])
 	x = [item[0] for item in dataset]
 	y = [item[1] for item in dataset]
 	return x, y

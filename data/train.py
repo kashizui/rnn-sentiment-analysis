761c1ee09f988354bb65a125569a4e3da55a88a0
Stephen Koo <stephen.theking@gmail.com>
2016-05-30 20:44:05 -0700

    Unify everything into train.py

--- data/train.py
+++ data/train.py
@@ -3,59 +3,98 @@
 Try `python train.py lstm` first.
 
 Usage:
-  train.py [--epochs=N --train-embedding --print-args --glove=FILE] <model>
+  train.py [--epochs=N --train-embedding --glove=FILE --data-cache=FILE --model=NAME --force-preprocess --parameters=FILE --evaluate-only --continue-training]
   train.py (-h | --help)
 
 Options:
   --epochs=N            Number of epochs [default: 10]
   --glove=FILE          Path to file with glove embedding to use. [default: glove.6B/glove.6B.50d.txt]
+  --model=NAME          Name of the model to use (should have a corresponding .py file in models package). [default: lstm]
+  --data-cache=FILE     Path to cached processed data. If file doesn't exist, reprocess data and save to this location. [default: sst_data.pkl]
+  --parameters=FILE     Path to saved parameters. Neither --continue-training nor --evaluate-only are provided, the parameters will be overwritten. [default: lstm.tflearn]
+  --continue-training   Continue training with the saved parameters.
+  --evaluate-only       Skip training and only evaluate the saved model.
   --train-embedding     Train the word embedding along with model.
-  --print-args          Print arguments.
+  --force-preprocess    Delete the cached data if it exists and preprocess the data again.
   -h --help             Show this screen.
 """
 from __future__ import division, print_function
+import cPickle as pkl
+import os
 
 from docopt import docopt
+from sklearn.metrics import classification_report
 import tflearn
 
 import utils
 import models
 
 
-def main():
-    args = docopt(__doc__)
-    if args['--print-args']:
-        for k, v in args.iteritems():
-            print("{:<20}: {!r:<10}".format(k, v))
-    epochs = int(args['--epochs'])
-    model_name = args['<model>']
-    train_embedding = args['--train-embedding']
-    data = utils.load_sst('sst_data.pkl')
-
+def load_data(args):
     print("Loading glove vectors...")
     glove = utils.build_glove(args['--glove'])
 
-    print("Loading model definition for %s..." % model_name)
-    net = models.get_model(model_name, embedding_size=glove[0].shape,
-                           train_embedding=train_embedding)
+    if not args['--force-preprocess'] and os.path.exists(args['--data-cache']):
+        print("Loading preprocessed data...")
+        with open(args['--data-cache']) as f:
+            data = pkl.load(f)
+    else:
+        print("Processing treebank dataset...")
+        data = utils.load_sst(glove)
+        print("Caching preprocessed data...")
+        with open(args['--data-cache'], 'w') as f:
+            pkl.dump(data, f)
+    return data, glove
+
+
+def train(args, glove, data):
+    print("Loading model definition for %s..." % args['--model'])
+    net = models.get_model(args['--model'], embedding_size=glove[0].shape,
+                           train_embedding=args['--train-embedding'])
     model = tflearn.DNN(net, clip_gradients=0., tensorboard_verbose=0)
 
-    print("Initializing word embedding...")
-    # Retrieve embedding layer weights (only a single weight matrix, so index is 0)
-    embedding_weights = tflearn.get_layer_variables_by_name('EmbeddingLayer')[0]
-    # Initialize with glove embedding
-    model.set_weights(embedding_weights, glove[0])
+    if args['--evaluate-only'] or args['--continue-training']:
+        print("Loading saved parameters..")
+        model.load(args['--parameters'])
+    else:
+        print("Initializing word embedding...")
+        # Retrieve embedding layer weights (only a single weight matrix, so index is 0)
+        embedding_weights = tflearn.get_layer_variables_by_name('EmbeddingLayer')[0]
+        # Initialize with glove embedding
+        model.set_weights(embedding_weights, glove[0])
 
-    print("Training...")
-    model.fit(data.trainX, data.trainY,
-              n_epoch=epochs,
-              validation_set=(data.valX, data.valY),
-              show_metric=True, batch_size=128)
+    if not args['--evaluate-only']:
+        print("Training...")
+        model.fit(data.trainX, data.trainY,
+                  n_epoch=int(args['--epochs']),
+                  validation_set=(data.valX, data.valY),
+                  show_metric=True, batch_size=128)
 
-    print("Saving Model...")
-    model_path = '%s.tflearn' % args['<model>']
-    model.save(model_path)
-    print("Saved model to %s" % model_path)
+        print("Saving parameters to %r..." % args['--parameters'])
+        model.save(args['--parameters'])
+
+    return model
+
+
+def evaluate(args, model, data):
+    test_predict = model.predict(data.testX)
+
+    y_pred = [utils.get_sentiment(e[0]) for e in test_predict]
+    y_true = [e[0] for e in data.testY]
+
+    print(classification_report(y_true, y_pred))
+
+
+def main():
+    args = docopt(__doc__)
+
+    # Print argument list for debugging
+    for k, v in args.iteritems():
+        print("{:<20}: {!r:<10}".format(k, v))
+
+    data, glove = load_data(args)
+    model = train(args, glove, data)
+    evaluate(args, model, data)
 
 
 if __name__ == '__main__':
--- data/utils.py
+++ data/utils.py
@@ -18,38 +18,10 @@ def get_sentiment(val):
     return int(val >= 0.5)
 
 
-def format_data(dataset):
+def unzip_examples(dataset):
     x = [item[0] for item in dataset]
     y = [item[1] for item in dataset]
     return x, y
-
-
-def load_sst(fname):
-    """Loads processed Stanford Sentiment Treebank data."""
-    print("Loading Data...")
-    data = pkl.load(open(fname, "rb"))
-    train = data['train']
-    test = data['test']
-    val = data['dev']
-
-    trainX, trainY = format_data(train[1:])
-    valX, valY = format_data(val[1:])
-    testX, testY = format_data(test[1:])
-
-    # Data preprocessing
-    # Sequence padding
-    print("Padding Sequences...")
-    trainX = pad_sequences(trainX, maxlen=200, value=0.)
-    valX = pad_sequences(valX, maxlen=200, value=0.)
-    testX = pad_sequences(testX, maxlen=200, value=0.)
-
-    # Converting labels to binary vectors
-    print("Converting labels to binary vectors...")
-    trainY = to_categorical(trainY, nb_classes=2)
-    valY = to_categorical(valY, nb_classes=2)
-    testY = to_categorical(testY, nb_classes=2)
-
-    return Dataset(trainX, trainY, valX, valY, testX, testY)
 
 
 def build(src_filename, delimiter=',', header=True, quoting=csv.QUOTE_MINIMAL):
@@ -123,3 +95,154 @@ def glove2dict(src_filename):
 def randvec(n=50, lower=-0.5, upper=0.5):
     """Returns a random vector of length `n`. `w` is ignored."""
     return np.array([random.uniform(lower, upper) for i in range(n)])
+
+NUM_EXAMPLES = float('inf')
+
+
+def split_database(list_to_split, split_fname):
+    """
+    Split a list of sentences into train, test, and dev groups.
+
+    Based on the index specified in split_fname, the sentences will
+    be grouped according to index into lists.
+
+    Arguments:
+    ----------
+        list_to_split: the list to be split into groups.
+        split_fname: name of file with one line for each list element
+
+    Returns:
+    ----------
+        train: Training set as divided by split_fname
+        test: Test set as divided by split_fname
+        dev: Dev set as divided by split_fname
+    """
+    train = []
+    test = []
+    dev = []
+    with open(split_fname, 'rb') as split:
+        next(split)
+        for line in split:
+            index, group = line.split(',')
+            group = int(group.strip())
+            if index in list_to_split:
+                if group == 1:
+                    train.append(list_to_split[index])
+                elif group == 2:
+                    test.append(list_to_split[index])
+                elif group == 3:
+                    dev.append(list_to_split[index])
+    return train, test, dev
+
+
+def phrases2ints(word_dict, dataset_sentences_fname):
+    """
+    Convert a dataset of sentences into numbers corresponding to the words in the sentence.
+
+    Arguments:
+    ----------
+        word_dict: Dict relating words to their indices
+        dataset_sentences_fname: File name that holds the sentences in the dataset.
+
+    Returns:
+    ----------
+        int_phrases: Returns phrases with ints substituted for words corresponding to their index
+                        in dataset_sentences_fname
+
+    """
+    with open(dataset_sentences_fname, 'rb') as dataset_sentences:
+        int_phrases = []
+        next(dataset_sentences)
+        num_lines_read = 0
+        for sentence in dataset_sentences:
+            if num_lines_read >= NUM_EXAMPLES:
+                break
+
+            index, phrase = sentence.split('\t')
+            int_phrase = tuple(
+                word_dict.get(word.lower(), 0)  # zero doesn't really make sense here
+                for word in phrase.split()
+            )
+            int_phrases.append((int_phrase, index))
+            num_lines_read += 1
+
+    return int_phrases
+
+
+def get_phrases_dict(phrases_fname):
+    """
+    Convert a file with phrases into a phrase dict and a word dict.
+
+    Arguments:
+    ----------
+        phrases_fname: Name of file that holds phrases and their corresponding index.
+
+    Returns:
+    ----------
+        phrase_dict: Dict of phrases and their corresponding index
+        word_dict: subset of phrase_dict that only contains single words
+    """
+    phrase_dict = {}
+    with open(phrases_fname, 'rb') as phrases:
+        for line in phrases.readlines():
+            phrase, index = line.split('|')
+            phrase_dict[phrase] = int(index.strip())
+    word_dict = {key: val for key, val in phrase_dict.items() if len(key.split())==1}
+    return phrase_dict, word_dict
+
+
+def indices_to_sentiment(int_phrases, sentiment_labels_fname):
+    with open(sentiment_labels_fname, 'rb') as sentiment_labels:
+        next(sentiment_labels)
+        label_dict = dict(tuple(item.split('|')) for item in sentiment_labels)
+
+    return {
+        idx: (phrase, get_sentiment(float(label_dict[idx])))  # FIXME
+        for phrase, idx in int_phrases
+        if idx.isdigit()
+    }
+
+
+def glove_word_indices(glove_data):
+    words = glove_data[1]
+    return {word: i for i, word in enumerate(words)}
+
+
+def load_sst(glove_data):
+    # Get the phrases and their indices
+    print("Getting Phrase Dictionary...")
+    phrase_dict, _ = get_phrases_dict('stanfordSentimentTreebank/dictionary.txt')
+
+    print("Getting glove word indices...")
+    word_dict = glove_word_indices(glove_data)
+
+    # Convert the phrases to ints with word indices so they can be processed by Neural Network
+    print("Converting to Ints...")
+    int_phrases = phrases2ints(word_dict, 'stanfordSentimentTreebank/datasetSentences.txt')
+
+    # Convert indices to sentiment values
+    print("Converting Indices to Sentiment Values...")
+    phrase_sentiments = indices_to_sentiment(int_phrases, 'stanfordSentimentTreebank/sentiment_labels.txt')
+
+    # Split into train, test, and dev groups
+    print("Splitting into train, test, and dev groups...")
+    train, test, val = split_database(phrase_sentiments, 'stanfordSentimentTreebank/datasetSplit.txt')
+
+    # Unzip input sequences and sentiment labels
+    trainX, trainY = unzip_examples(train)
+    valX, valY = unzip_examples(val)
+    testX, testY = unzip_examples(test)
+
+    # Sequence padding
+    print("Padding Sequences...")
+    trainX = pad_sequences(trainX, maxlen=200, value=0.)
+    valX = pad_sequences(valX, maxlen=200, value=0.)
+    testX = pad_sequences(testX, maxlen=200, value=0.)
+
+    # Converting labels to binary vectors
+    print("Converting labels to binary vectors...")
+    trainY = to_categorical(trainY, nb_classes=2)
+    valY = to_categorical(valY, nb_classes=2)
+    testY = to_categorical(testY, nb_classes=2)
+
+    return Dataset(trainX, trainY, valX, valY, testX, testY)

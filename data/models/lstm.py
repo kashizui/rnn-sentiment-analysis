53368b5dc7b4d142b8e18131b0363039bb684ccf
Stephen Koo <stephen.theking@gmail.com>
2016-06-02 23:25:33 -0700

    Allow hidden dim to be varied

--- data/models/lstm.py
+++ data/models/lstm.py
@@ -20,12 +20,12 @@ from __future__ import division, print_function, absolute_import
 import tflearn
 
 
-def build(embedding_size=(400000, 50), train_embedding=False):
+def build(embedding_size=(400000, 50), train_embedding=False, hidden_dims=128):
     net = tflearn.input_data([None, 200])
     net = tflearn.embedding(net, input_dim=embedding_size[0],
                             output_dim=embedding_size[1],
                             trainable=train_embedding, name='EmbeddingLayer')
-    net = tflearn.lstm(net, 128)
+    net = tflearn.lstm(net, hidden_dims)
     net = tflearn.dropout(net, 0.5)
     net = tflearn.fully_connected(net, 2, activation='softmax')
     net = tflearn.regression(net, optimizer='adam',
--- data/train.py
+++ data/train.py
@@ -1,9 +1,9 @@
 """Train model for sentiment analysis.
 
-Try `python train.py lstm` first.
-
 Usage:
-  train.py [--epochs=N --train-embedding --glove=FILE --data-cache=FILE --model=NAME --force-preprocess --parameters=FILE --evaluate-only --continue-training]
+  train.py [--epochs=N] [--train-embedding] [--glove=FILE] [--data-cache=FILE]
+           [--model=NAME] [--force-preprocess] [--parameters=FILE]
+           [--evaluate-only] [--continue-training] [--hidden-dims=N]
   train.py (-h | --help)
 
 Options:
@@ -12,6 +12,7 @@ Options:
   --model=NAME          Name of the model to use (should have a corresponding .py file in models package). [default: lstm]
   --data-cache=FILE     Path to cached processed data. If file doesn't exist, reprocess data and save to this location. [default: sst_data.pkl]
   --parameters=FILE     Path to saved parameters. Neither --continue-training nor --evaluate-only are provided, the parameters will be overwritten. [default: lstm.tflearn]
+  --hidden-dims=N        Number of dimensions to use in the hidden state. [default: 128]
   --continue-training   Continue training with the saved parameters.
   --evaluate-only       Skip training and only evaluate the saved model.
   --train-embedding     Train the word embedding along with model.
@@ -31,7 +32,7 @@ import models
 
 
 def load_data(args):
-    print("Loading glove vectors...")
+    print("Loading glove vectors from %r..." % args['--glove'])
     glove = utils.build_glove(args['--glove'])
 
     if not args['--force-preprocess'] and os.path.exists(args['--data-cache']):
@@ -50,7 +51,8 @@ def load_data(args):
 def train(args, glove, data):
     print("Loading model definition for %s..." % args['--model'])
     net = models.get_model(args['--model'], embedding_size=glove[0].shape,
-                           train_embedding=args['--train-embedding'])
+                           train_embedding=args['--train-embedding'],
+                           hidden_dims=int(args['--hidden-dims']))
     model = tflearn.DNN(net, clip_gradients=0., tensorboard_verbose=0)
 
     if args['--evaluate-only'] or args['--continue-training']:
@@ -78,17 +80,18 @@ def train(args, glove, data):
 
 def evaluate(args, model, data):
     train_predict = model.predict(data.trainX)
+    import pdb; pdb.set_trace()
     print("TRAINING RESULTS")
     print(classification_report(
-        [utils.get_sentiment(e[0]) for e in train_predict],
-        [e[0] for e in data.trainY]))
+        [utils.get_sentiment(e[1]) for e in train_predict],
+        [e[1] for e in data.trainY]))
     print()
 
     test_predict = model.predict(data.testX)
     print("TEST RESULTS")
     print(classification_report(
-        [utils.get_sentiment(e[0]) for e in test_predict],
-        [e[0] for e in data.testY]))
+        [utils.get_sentiment(e[1]) for e in test_predict],
+        [e[1] for e in data.testY]))
     print()
 
 
